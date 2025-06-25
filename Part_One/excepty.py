82c3f23e592b868a733c164e8f6edaa95e5818de
Sstarr <sstarr@jesi.jesi>
2025-06-25 17:14:03 +0100

    Updated

---
+++ Part_One/excepty.py
@@ -0,0 +1,10 @@
+try:
+    user_age = int(input("Enter your age: "))
+    income = 10000
+    risk = income / user_age
+    print(f"Your risk factor is: {risk}")
+except ZeroDivisionError:
+    print("Age cannot be zero. Please enter a valid age.")
+    print(user_age)
+except ValueError:
+    print("Invalid input. Please enter a number.")
